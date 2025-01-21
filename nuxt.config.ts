e273909a87fa096bae7689a4a3817583b1fc42da
Nam Nguyen Hai <doluadao1995@gmail.com>
2025-01-22 00:16:59 +0100

    chore: add basics libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+    '@vee-validate/nuxt',
+    '@nuxt/icon',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-01-21'
 })
